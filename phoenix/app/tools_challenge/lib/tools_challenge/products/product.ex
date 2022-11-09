bc0cabb2382c7179cb8a7c1db35e0124186dfcc8
Douglas Lenfers <douglaslenfers@gmail.com>
2022-11-09 11:34:26 -0300

    validate barcode min 8 max 13

--- phoenix/app/tools_challenge/lib/tools_challenge/products/product.ex
+++ phoenix/app/tools_challenge/lib/tools_challenge/products/product.ex
@@ -23,6 +23,7 @@ defmodule ToolsChallenge.Products.Product do
     |> cast(attrs, [:sku, :name, :description, :quantity, :price, :barcode])
     |> validate_required([:sku, :name, :price, :barcode])
     |> validate_length(:description, max: 255)
+    |> validate_length(:barcode, min: 8, max: 13)
     |> validate_number(:price, greater_than: 0)
   end
 
