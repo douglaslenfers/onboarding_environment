f7af0e595fb666503432207aa192d040c0a90cdf
Douglas Lenfers <douglaslenfers@gmail.com>
2022-11-21 18:08:52 -0300

    added dashboard for exq job manager

--- phoenix/app/tools_challenge/lib/tools_challenge_web/router.ex
+++ phoenix/app/tools_challenge/lib/tools_challenge_web/router.ex
@@ -13,6 +13,14 @@ defmodule ToolsChallengeWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :exq do
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:put_secure_browser_headers)
+    plug(ExqUi.RouterPlug, namespace: "exq")
+  end
+
   scope "/", ToolsChallengeWeb do
     pipe_through :browser
 
@@ -20,18 +28,11 @@ defmodule ToolsChallengeWeb.Router do
     resources "/products", ProductController
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ToolsChallengeWeb do
-  #   pipe_through :api
-  # end
+  scope "/exq", ExqUi do
+    pipe_through(:exq)
+    forward("/", RouterPlug.Router, :index)
+  end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
